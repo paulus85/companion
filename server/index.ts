443f0bd22dd36d5e7580b160dade806e1fc53d97
Paul Rivière <paul.riviere@thetribe.io>
2023-03-03 15:47:56 +0100

    chore(server): create the eslint configuration

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,9 @@
-import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import app from "./src/app";
 
 dotenv.config();
 
-const app = express();
 const port = process.env.PORT;
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);

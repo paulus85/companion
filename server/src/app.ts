80d9c306d766b046d3af7473bd74544ec0dc8041
Paul Rivière <paul.riviere@thetribe.io>
2023-03-03 22:18:50 +0100

    feat(server): start the implementation of socket.io

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,22 @@
 import express, { Request, Response } from "express";
+import { Server } from "socket.io";
+import { createServer } from "http";
 
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+  res.send({
+    uptime: process.uptime(),
+    message: "OK",
+    timestamp: Date.now(),
+  });
 });
 
-export default app;
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  console.log(socket.id);
+});
+
+export default httpServer;
